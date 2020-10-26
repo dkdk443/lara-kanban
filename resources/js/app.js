1f65d6ea979f7d2d3b7d93c78b133f8fb380079c
dkdk443 <demo_weeeds_470@yahoo.co.jp>
2020-10-26 21:39:59 +0900

    bootstrap-vueの導入

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,11 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
+
 require('./bootstrap');
 
 window.Vue = require('vue');
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 /**
  * The following block of code may be used to automatically register your
@@ -18,6 +20,14 @@ window.Vue = require('vue');
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
--- resources/js/components/TasklistComponent.vue
+++ resources/js/components/TasklistComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    task
+   
   </div>
 </template>
 
